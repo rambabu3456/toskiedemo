73afee6bc5622457cb11fda6472b5c806c4c850f
rambabu3456 <74294951+rambabu3456@users.noreply.github.com>
2021-11-30 01:04:35 +0530

    Update main.py
    updated

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-Hello
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+service = client.verify.services.create(
+                                     friendly_name='My First Verify Service'
+                                 )
+
+print(service.sid)
